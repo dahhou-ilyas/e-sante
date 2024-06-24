2f7b4e66a984ed8faf5a0d4928014d35ea793fd4
dahhou ilyas <dahhou.ilyas@gmail.com>
2024-06-24 23:39:38 +0100

    add repository

---
+++ src/main/java/com/example/backend/repository/AntecedentFamilialRepository.java
@@ -0,0 +1,7 @@
+package com.example.backend.repository;
+
+import com.example.backend.entities.AntecedentFamilial;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AntecedentFamilialRepository extends JpaRepository<AntecedentFamilial,Long> {
+}
---
+++ src/main/java/com/example/backend/repository/AntecedentPersonnelRepository.java
@@ -0,0 +1,7 @@
+package com.example.backend.repository;
+
+import com.example.backend.entities.AntecedentPersonnel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AntecedentPersonnelRepository extends JpaRepository<AntecedentPersonnel,Long> {
+}
